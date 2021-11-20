65d24ad9497c8f8467741e8f0c50cc48bd0358a8
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-11-20 13:52:02 +0600

    update get

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
-const port = process.env.PORT || 5000
+
+const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sovrt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -15,7 +17,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('database connected successfully');
+
+        const database = client.db('blood-buddies');
+        const bloodPostReqCollection = database.collection('bloodPostReq');
+
+
+        //Blood Get Api
+        app.get('/bloodPostReq', async (req, res) => {
+            const cursor = bloodPostReqCollection.find({});
+            const bloodPostReq = await cursor.toArray();
+            res.send(bloodPostReq);
+        })
+
+
+        //Blood POST Api
+        app.post('/bloodPostReq', async (req, res) => {
+            const bloodPostReq = req.body;
+            const result = await bloodPostReqCollection.insertOne(bloodPostReq);
+            console.log(bloodPostReq);
+            res.json(result)
+        })
 
     } finally {
         //await client.close();
